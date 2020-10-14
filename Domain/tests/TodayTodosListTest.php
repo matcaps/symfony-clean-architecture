ebe168f40ba4fba8cbe9ae91803df83d64572d9e
spackydev <mathieu.capon@gmail.com>
2020-10-14 22:06:30 +0200

    (style) PhpCs fix

--- Domain/tests/TodayTodosListTest.php
+++ Domain/tests/TodayTodosListTest.php
@@ -1,8 +1,5 @@
 <?php
 
-#devrait renvoyer la liste des todos qui doivent être cloturés le jour meme
-#devrait renvoyer la liste des todos qui doivent etre terminés le jour même ou précédemment
-
 use App\Infrastructure\Ports\Secondary\TodosRepository;
 use MatCaps\Beta\Domain\Entity\Todo;
 
@@ -10,40 +7,51 @@ use function PHPUnit\Framework\assertCount;
 
 it(
     "should present a list of 10 todos which should be closed today",
-    function (TodosRepository $repo) {
-        $todos = $repo->findAllDueToday();
+    function () {
+        $repo1 = new TodosRepository();
+
+        for ($i = 1; $i < 11; $i++) {
+            $todo = new Todo("Todo message {$i}", new DateTimeImmutable());
+            $repo1->add($todo);
+        }
+
+        $todos = $repo1->findAllDueToday();
         assertCount(10, $todos);
     }
-)->with([todoFromTodayRepositoryProvider()]);
+);
 
 it(
     "should not find todos which have to be closed today",
-    function (TodosRepository $repo) {
-        $todos = $repo->findAllDueToday();
-        assertCount(0, $todos);
+    function () {
+        $repo = new TodosRepository();
+
+        for ($i = 1; $i < 11; $i++) {
+            $todo = new Todo("Todo message {$i}", (new DateTimeImmutable())->add(new DateInterval("P2D")));
+            $repo->add($todo);
+        }
+        $todosNotDue = $repo->findAllDueToday();
+
+        assertCount(0, $todosNotDue);
     }
-)->with([todoFromAfterTodayRepositoryProvider()]);
+);
 
-function todoFromTodayRepositoryProvider()
-{
-    $repo = new TodosRepository();
+//it(
+//    "should not find todos which have to be closed today",
+//    function (TodosRepository $repo) {
+//        $todosNotDue = $repo->findAllDueToday();
+//        assertCount(0, $todosNotDue);
+//    }
+//)->with([todoFromAfterTodayRepositoryProvider()]);
 
-    for ($i = 1; $i < 11; $i++) {
-        $todo = new Todo("Todo message {$i}", new DateTimeImmutable());
-        $repo->add($todo);
-    }
 
-    return $repo;
-}
-
-function todoFromAfterTodayRepositoryProvider()
-{
-    $repo = new TodosRepository();
-
-    for ($i = 1; $i < 11; $i++) {
-        $todo = new Todo("Todo message {$i}", (new DateTimeImmutable())->add(new DateInterval("P2D")));
-        $repo->add($todo);
-    }
-
-    return $repo;
-}
+//function todoFromAfterTodayRepositoryProvider()
+//{
+//    $repo = new TodosRepository();
+//
+//    for ($i = 1; $i < 11; $i++) {
+//        $todo = new Todo("Todo message {$i}", (new DateTimeImmutable())->add(new DateInterval("P2D")));
+//        $repo->add($todo);
+//    }
+//
+//    return $repo;
+//}
